7bfc6d9198d322985dc7739cae027e4fbedaa41b
Bob Shelton <inhortte@gmail.com>
2015-09-21 20:44:57 +0200

    watchify is the best option here

--- bin/watch.sh
+++ bin/watch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/usr/local/bin/watchify public/js/vdnamenu.js -d -v -t reactify -g livereactload -o public/js/vdnabundle.js &
+/usr/local/bin/watchify public/js/vdnamenu.js -v -t reactify -g livereactload -o public/js/vdnabundle.js &
 node_modules/.bin/livereactload monitor -n public/js/vdnabundle.js
 wait
